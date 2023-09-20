8113ee84eeec46bef26d965fbf0c74979a53761c
Yuvaraj Nagi <40654321+yuvi55@users.noreply.github.com>
2023-09-20 17:18:25 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,89 @@
+let data = [
+    [{
+        name: 'England Vs New Zealand',
+        date: 'Oct 5',
+        venue: 'Ahmedabad',
+        image: '../../images/match_1.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75413/eng-vs-nz-1st-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      },
+      {
+        name: 'Pakistan vs Netherlands',
+        date: 'Oct 6',
+        venue: 'Hyderabad',
+        image: '../../images/match_2.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75420/pak-vs-ned-2nd-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      },
+      {
+        name: 'Bangladesh vs Afghanistan',
+        date: 'Oct 7',
+        venue: 'Dharamsala',
+        image: '../../images/match_3.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75427/ban-vs-afg-3rd-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      }],
+      [{
+        name: 'South Africa vs Sri Lanka',
+        date: 'Oct 7',
+        venue: 'Delhi',
+        image: '../../images/image_4.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75434/rsa-vs-sl-4th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      },
+      {
+        name: 'India vs Australia',
+        date: 'Oct 8',
+        venue: 'Chennai',
+        image: '../../images/match_5.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75437/ind-vs-aus-5th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      },
+      {
+        name: 'New Zealand vs Netherlands',
+        date: 'Oct 9',
+        venue: 'Hyderabad',
+        image: '../../images/match_6.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75444/nz-vs-ned-6th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      }],
+      [{
+        name: 'England vs Bangladesh',
+        date: 'Oct 10',
+        venue: 'Dharamsala',
+        image: '../../images/match_7.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75451/eng-vs-ban-7th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      },
+      {
+        name: 'Pakistan vs Sri Lanka',
+        date: 'Oct 10',
+        venue: 'Hyderabad',
+        image: '../../images/match_8.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75462/pak-vs-sl-8th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      },
+      {
+        name: 'India vs Afghanistan',
+        date: 'Oct 11',
+        venue: 'Delhi',
+        image: '../../images/image_9.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75458/ind-vs-afg-9th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      }],
+      [{
+        name: 'Australia vs South Africa',
+        date: 'Oct 12',
+        venue: 'Lucknow',
+        image: '../../images/match_10.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75465/aus-vs-rsa-10th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      },
+      {
+        name: 'New Zealand vs Bangladesh',
+        date: 'Oct 13',
+        venue: 'Chennai',
+        image: '../../images/match_11.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75469/nz-vs-ban-11th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      },
+      {
+        name: 'India vs Pakistan',
+        date: 'Oct 14',
+        venue: 'Ahmedabad',
+        image: '../../images/match_12.jpeg',
+        link: 'https://www.cricbuzz.com/cricket-match-facts/75476/ind-vs-pak-12th-match-icc-cricket-world-cup-2023', // You can leave this blank for now
+      }]
+
+]
+
+export default data
